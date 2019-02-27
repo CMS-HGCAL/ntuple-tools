563c57434eaa52c7f8cdfce7cfe487e2dca52f0d
jniedzie <jeremi.niedziela@cern.ch>
2019-02-27 15:19:46 +0100

    Copying default config with a random file name for scoreVsParam to allow parallel execution.

--- cppVersion/src/scoreVsParam.cpp
+++ cppVersion/src/scoreVsParam.cpp
@@ -21,15 +21,15 @@ EParam paramNum = kCriticalDistanceEE;
 double testParamStep = 0.5;
 
 const double severityFactor = 10.0;
-const string configPath = "testScore.md";
-
-auto config = my::make_unique<ConfigurationManager>(configPath);
+const string baseConfigPath = "testScore.md";
+string configPath;
 
 double GetScore()
 {
 	string command = "./createQualityPlots " + configPath;
 	system(command.c_str());
 	
+	auto config = my::make_unique<ConfigurationManager>(configPath);
 	ClusteringOutput output = ReadOutput(config->GetScoreOutputPath());
 	
 	double distance = fabs(output.containmentMean-1)
@@ -77,7 +77,11 @@ int main(int argc, char* argv[])
 																								(paramMax[paramNum]-paramMin[paramNum])/testParamStep+1,
 																								paramMin[paramNum], paramMax[paramNum]);
 	
-	double paramSave = GetDoubleFromConfig(configPath, testParam);
+	// Copy default config with some random name
+	srand((uint)time(NULL));
+	int r = RandInt(100, 10000000);
+	configPath = "testScore_"+to_string(r)+".md";
+	system(("cp "+baseConfigPath+" "+configPath).c_str());
 	
 	for(double param = paramMin[paramNum]; param < paramMax[paramNum]; param += testParamStep){
 		UpdateParamValue(configPath, testParam, param);
@@ -88,7 +92,7 @@ int main(int argc, char* argv[])
 	}
 	
 	// Save the initial value back in the config
-	UpdateParamValue(configPath, testParam, paramSave);
+	system(("rm "+configPath).c_str());
 	
 	scoreVsParamHist->SaveAs(("score_vs_"+testParam+".root").c_str());
 	return 0;
