32f8a931fba3711fa3f721f90f383689eedd6b4b
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2019-03-06 10:04:54 +0100

    Fixing runnin on batch + updating scoreVsParam

--- cppVersion/run_test_score.sh
+++ cppVersion/run_test_score.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-echo "Starting Genetic optimizer"
+echo "Starting testing score"
 
 export lcgenv=/cvmfs/sft.cern.ch/lcg/releases/lcgenv/0.2-bbd0d/x86_64-centos7-gcc48-dbg/lcgenv
 
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-centos7-gcc48-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/releases/pcre/8.34-2c9d9/x86_64-centos7-gcc49-opt/pcre-env.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+
 echo "Which root:`which root`"
 echo "Lib path:`echo $LD_LIBRARY_PATH`"
 
 cd /afs/cern.ch/work/j/jniedzie/private/hgcal/ntuple-tools/cppVersion/
 
 echo "Im in `pwd`"
-./geneticOptimizer
+./scoreVsParam $1 $2
--- cppVersion/src/scoreVsParam.cpp
+++ cppVersion/src/scoreVsParam.cpp
@@ -65,15 +65,21 @@ double GetScore()
 int main(int argc, char* argv[])
 {
 	if(argc != 3){
-		cout<<"Usage: param_name step"<<endl;
+		cout<<"Usage: output_path param_name step"<<endl;
 		cout<<"\nwhere param name can be:"<<endl;
 		cout<<"\tcritical_distance_EE\n\tcritical_distance_FH\n\tcritical_distance_BH"<<endl;
     cout<<"\tassignment_distance_EE\n\tassignment_distance_FH\n\tassignment_distance_BH"<<endl;
 		cout<<"\tdeltac_EE\n\tdeltac_FH\n\tdeltac_BH\n\tkappa"<<endl;
+		
+		cout<<"Arguments passed:"<<endl;
+		for(int i=0;i<argc;i++){
+			cout<<argv[i]<<endl;
+		}
 		exit(0);
 	}
-	string testParam = argv[1];
-	double testParamStep = stod(argv[2]);
+	string outputPath = argv[1];
+	string testParam = argv[2];
+	double testParamStep = 0.5;//stod(argv[3]);
 	
 	EParam paramNum;
 	if(			testParam == "critical_distance_EE") 	  paramNum = kCriticalDistanceEE;
@@ -96,19 +102,17 @@ int main(int argc, char* argv[])
 	cout<<"Starting score vs. param"<<endl;
 	gROOT->ProcessLine(".L loader.C+");
 	
-	auto scoreVsParamHist = my::make_unique<TH1D>(("score vs "+testParam).c_str(),
-																								("score vs "+testParam).c_str(),
+	auto scoreVsParamHist = my::make_unique<TH1D>(("score_vs_"+testParam).c_str(),
+																								("score_vs_"+testParam).c_str(),
 																								(paramMax[paramNum]-paramMin[paramNum])/testParamStep+1,
 																								paramMin[paramNum], paramMax[paramNum]);
 	
-	// Copy default config with some random name
-	srand((uint)time(NULL));
-	int r = RandInt(100, 10000000);
-	configPath = "testScore_"+to_string(r)+".md";
-	system(("cp "+baseConfigPath+" "+configPath).c_str());
+	// Copy default config
+	configPath = outputPath+"/testScore_"+testParam+".md";
+	system(("cp "+outputPath+"/"+baseConfigPath+" "+configPath).c_str());
 	
 	// Put random score output path in the random config
-	string scoreOutputPath = "testScore_"+to_string(r)+".out";
+	string scoreOutputPath = outputPath+"/testScore_"+testParam+".out";
 	UpdateParamValue(configPath, "score_output_path", scoreOutputPath);
 	
 	for(double param = paramMin[paramNum]; param < paramMax[paramNum]; param += testParamStep){
@@ -123,7 +127,7 @@ int main(int argc, char* argv[])
 	system(("rm "+configPath).c_str());
 	system(("rm "+scoreOutputPath).c_str());
 	
-	scoreVsParamHist->SaveAs(("score_vs_"+testParam+".root").c_str());
+	scoreVsParamHist->SaveAs((outputPath+"/score_vs_"+testParam+".root").c_str());
 	return 0;
 }
 
